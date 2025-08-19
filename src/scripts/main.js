f8b2a9a01f782993c9266d16815f9af9a9c4310f
Ruslan <gnatisin.ruslan1988@gmail.com>
2025-08-19 17:49:59 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,51 @@
 'use strict';
 
 // write code here
+const headers = [...document.querySelectorAll('table thead tr th')];
+
+headers.forEach((header) => {
+  header.addEventListener('click', () => {
+    const indexHeader = headers.indexOf(header);
+    const rows = [...document.querySelectorAll('table tbody tr')];
+
+    let sortedRows;
+
+    if (header.textContent === 'Name' || header.textContent === 'Position') {
+      sortedRows = [...rows].sort((a, b) => {
+        return a.children[indexHeader].textContent
+          .trim()
+          .localeCompare(b.children[indexHeader].textContent.trim());
+      });
+    }
+
+    if (header.textContent === 'Age') {
+      sortedRows = [...rows].sort((a, b) => {
+        const aAge = Number(a.cells[2].textContent);
+        const bAge = Number(b.cells[2].textContent);
+
+        return aAge - bAge;
+      });
+    }
+
+    if (header.textContent === 'Salary') {
+      sortedRows = [...rows].sort((a, b) => {
+        const aSalary = Number(
+          a.cells[3].textContent.replace(/[^0-9.-]+/g, ''),
+        );
+        const bSalary = Number(
+          b.cells[3].textContent.replace(/[^0-9.-]+/g, ''),
+        );
+
+        return aSalary - bSalary;
+      });
+    }
+
+    const tbody = document.querySelector('table tbody');
+
+    tbody.innerHTML = '';
+
+    sortedRows.forEach((row) => {
+      tbody.append(row);
+    });
+  });
+});
